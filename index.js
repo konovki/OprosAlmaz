2f7b3b7c9d0ac44d6e984605e8330da709ac50bf
konovki <67338565+konovki@users.noreply.github.com>
2025-01-20 16:51:14 +0300

    Update index.js
    1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;// || 3001;
 
 app.use(express.static('public')); // Раздача статических файлов из папки public
 
